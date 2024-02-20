1892effce4411783d660e66ec7ab53c360b92ab1
keeq0 <52675873+keeq0@users.noreply.github.com>
2024-02-20 20:14:57 +0300

    актуалочка

--- site/src/components/Menu.js
+++ site/src/components/Menu.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import '../css/menu.css';
 
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const Menu = ({header, items, active, setActive}) => {
+const Menu = ({ header, items, active, setActive }) => {
+    const handleItemClick = () => {
+        setActive(false); // Установить значение active в false при клике на ссылку
+    };
+
     return (
         <div className={active ? 'menu active' : 'menu'}>
             <div className='blur'>
                 <div className='menu__content'>
                     <p className='menu__header'>{header}</p>
                     <ul className='menu__header--list'>
-                        {items.map (item => 
-                            <li className='menu__header--item'>
-                                <Link className='menu__header--link' to={item.href}>{item.value}</Link>
+                        {items.map(item =>
+                            <li className='menu__header--item' key={item.value}>
+                                <Link className='menu__header--link' to={item.href} onClick={handleItemClick}>
+                                    {item.value}
+                                </Link>
                             </li>
-                            )}
+                        )}
                     </ul>
                 </div>
             </div>
