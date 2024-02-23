61d6d532e14309b6eeef006f1874deafac0e6b07
airvt1x <95149637+airvt1x@users.noreply.github.com>
2024-02-23 21:21:26 +0300

    Add files via upload

---
+++ backend/controllers/UserContoller.js
@@ -0,0 +1,27 @@
+import UserModel from '../models/User.js';
+
+
+export const register = async (req, res) => {
+    try {
+        const doc = new UserModel({
+            Name: req.body.name,
+            Surname: req.body.surname,
+            age: req.body.age,
+            email: req.body.email,
+            number: req.body.number,
+            password: req.body.password,
+            role: req.body.role
+        });
+
+        const user = await doc.save();
+
+        res.status(200).json({
+            message: 'Пользователь успешно зарегистрирован'
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: req,
+        });
+    }
+};
