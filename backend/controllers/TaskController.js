05407c615b50b5fd1939bab752036e4c6f52c5fe
Johnny <73528373+freeny-afk@users.noreply.github.com>
2024-02-27 02:31:17 +0300

    Update TaskController.js

--- backend/controllers/TaskController.js
+++ backend/controllers/TaskController.js
@@ -1,87 +1,103 @@
 import TaskModel from "../models/Task.js";
 
 export const create = async (req, res) => {
-    try {
-        const doc = new TaskModel({
-            title: req.body.title,
-            text: req.body.text,
-            type: req.body.type,
-            deadline: req.body.deadline,
-            category: req.body.category,
-            user: req.userId,
-            budget_type: req.body.budget_type,
-            budget: req.body.budget,
-            imageUrl: req.body.imageUrl
-        });
+  try {
+    const doc = new TaskModel({
+      title: req.body.title,
+      text: req.body.text,
+      type: req.body.type,
+      deadline: req.body.deadline,
+      category: req.body.category,
+      user: req.userId,
+      budget_type: req.body.budget_type,
+      budget: req.body.budget,
+      imageUrl: req.body.imageUrl,
+    });
 
-        const post = await doc.save();
+    const post = await doc.save();
 
-        res.json(post);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Не удалось создать карточку'
-        });
-    }
-}
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось создать карточку",
+    });
+  }
+};
 
-export const getTasks = async(req,res)=>{
-    try {
-        const tasks = await TaskModel.find().populate('user').exec();
+export const getTasks = async (req, res) => {
+  try {
+    const tasks = await TaskModel.find().populate("user").exec();
 
-        res.json(tasks);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Не удалось получить таски',
-        });
-        
-    }
-}
+    res.json(tasks);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось получить записи",
+    });
+  }
+};
 
-export const getOne = async(req,res)=>{
-    try {
-        const TaskId = req.params.id;
+export const getOne = async (req, res) => {
+  try {
+    const TaskId = req.params.id;
 
-        TaskModel.findOne({_id: TaskId}).then(task =>{
-            res.json(task)
-        })
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Не удалось таск найти',
-        });
-        
-    }
-}
+    TaskModel.findOne({ _id: TaskId }).then((task) => {
+      res.json(task);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось найти запись",
+    });
+  }
+};
 
-export const update = async(req, res) => {
-    try {
-        const TaskId = req.params.id;
-        
-        await TaskModel.updateOne({
-            _id: TaskId,
-        },
-        {
-            title: req.body.title,
-            text: req.body.text,
-            type: req.body.type,
-            deadline: req.body.deadline,
-            category: req.body.category,
-            user: req.userId,
-            budget_type: req.body.budget_type,
-            budget: req.body.budget,
-            imageUrl: req.body.imageUrl
-        },
-        );
+export const update = async (req, res) => {
+  try {
+    const TaskId = req.params.id;
 
-        res.json({
-            success: true,
-        })
-    } catch (err) {
-        res.status(500).json({
-            message: 'Не удалось обновить таск',
-        })
-        
-    }
-}
+    await TaskModel.updateOne(
+      {
+        _id: TaskId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        type: req.body.type,
+        deadline: req.body.deadline,
+        category: req.body.category,
+        user: req.userId,
+        budget_type: req.body.budget_type,
+        budget: req.body.budget,
+        imageUrl: req.body.imageUrl,
+      }
+    );
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Не удалось обновить запись",
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const TaskId = req.params.id;
+
+    TaskModel.findOneAndDelete({
+      _id: TaskId,
+    });
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось удалить запись",
+    });
+  }
+};
