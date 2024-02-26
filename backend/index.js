c598e97d26b2f79904f509d26c038b8a797ca77a
Johnny <73528373+freeny-afk@users.noreply.github.com>
2024-02-27 02:33:14 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,55 +1,71 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
+import { UserController, TaskController } from "./controllers/index.js";
 import {
-    UserController,
-    TaskContoller
-} from './controllers/index.js';
-import {
-    registerValidation,
-    loginValidation,
-    TaskCreateValidation
-} from './validations.js';
-import {
-    checkAuth,
-    handleValidationErrors
-} from "./utils/index.js"
-
+  registerValidation,
+  loginValidation,
+  TaskCreateValidation,
+} from "./validations.js";
+import { checkAuth, handleValidationErrors } from "./utils/index.js";
 
 mongoose.set("strictQuery", false);
 mongoose
-    .connect('mongodb+srv://admin:admin@cluster0.mrquvr1.mongodb.net/tutors?retryWrites=true&w=majority')
-    .then(() => console.log('DB ok'))
-    .catch((err) => console.log('DB error', err));
+  .connect(
+    "mongodb+srv://arsk0820:88005553535kK@cluster0.cjriws9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Yo salam')
+app.get("/", (req, res) => {
+  res.send("Yo salam");
 });
 
 app.listen(1234, (err) => {
-    if (err) {
-        return console.log(err)
-    }
+  if (err) {
+    return console.log(err);
+  }
 
-    console.log('runninnnnn')
+  console.log("runninnnnn");
 });
-
 
 app.use(express.json());
 
-app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
 
-app.post('/auth/register', handleValidationErrors, registerValidation, UserController.register);
-app.get('/auth/me', checkAuth, UserController.getInfo);
-app.post('/tasks', checkAuth, TaskCreateValidation, handleValidationErrors, TaskContoller.create);
-app.get('/tasks', TaskContoller.getTasks);
-app.get('/tasks/:id', TaskContoller.getOne);
+app.post(
+  "/auth/register",
+  handleValidationErrors,
+  registerValidation,
+  UserController.register
+);
 
-app.delete('/tasks/:id', (req, res) => {
+app.get("/auth/me", checkAuth, UserController.getInfo);
 
-});
-app.patch('/tasks/:id', checkAuth, TaskCreateValidation, handleValidationErrors, TaskContoller.update);
+app.post(
+  "/tasks",
+  checkAuth,
+  TaskCreateValidation,
+  handleValidationErrors,
+  TaskController.create
+);
 
+app.get("/tasks", TaskController.getTasks);
+app.get("/tasks/:id", TaskController.getOne);
+
+app.delete("/tasks/:id", checkAuth, TaskController.remove);
+app.patch(
+  "/tasks/:id",
+  checkAuth,
+  TaskCreateValidation,
+  handleValidationErrors,
+  TaskController.update
+);
 
 //app.post('/upload') // фотокарточки
