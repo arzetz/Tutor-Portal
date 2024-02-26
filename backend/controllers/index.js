205e102208e88931ecc3cc4644b6714a938e9e70
Johnny <73528373+freeny-afk@users.noreply.github.com>
2024-02-27 02:30:58 +0300

    Update index.js

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,2 +1,2 @@
-export * as UserController from "./UserContoller.js"
-export * as TaskContoller from "./TaskController.js"
+export * as UserController from "./UserController.js";
+export * as TaskController from "./TaskController.js";
